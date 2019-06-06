6a305b4c44b86163a4d6f46c26a3cc94ff3dcd62
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2019-06-06 14:26:20 -0300

    Add proper test for AutomataParser

--- parser/src/test/java/com/wawey/parser/AutomataParserTest.java
+++ parser/src/test/java/com/wawey/parser/AutomataParserTest.java
@@ -3,10 +3,7 @@ package com.wawey.parser;
 import com.wawey.lexer.Lexer;
 import com.wawey.lexer.LexerBuilder;
 import com.wawey.lexer.Token;
-import com.wawey.parser.ast.ASTNode;
-import com.wawey.parser.ast.ASTPrettyPrinter;
-import com.wawey.parser.ast.NonTerminalNode;
-import com.wawey.parser.ast.NumberLiteralNode;
+import com.wawey.parser.ast.*;
 import com.wawey.parser.automata.FileAutomata;
 import org.junit.Assert;
 import org.junit.Test;
@@ -55,7 +52,51 @@ public class AutomataParserTest {
         Lexer lexer = LexerBuilder.buildTSLexer();
         Parser parser = new AutomataParser(new FileAutomata());
         ASTNode result = parser.parse(lexer.lex("print(3+3);"));
-        ASTPrettyPrinter.prettyPrint(result);
+        ASTNode expected = new NonTerminalNode(
+                Rule.FILE,
+                new NonTerminalNode(
+                        Rule.PROGRAM,
+                        new NonTerminalNode(
+                                Rule.LINE,
+                                new NonTerminalNode(
+                                        Rule.STATEMENT,
+                                        new NonTerminalNode(
+                                                Rule.PRINT_STATEMENT,
+                                                new NonTerminalNode(
+                                                        Rule.ADDITIVE_EXPRESSION,
+                                                        new NonTerminalNode(
+                                                                Rule.SUM_EXPRESSION,
+                                                                new NonTerminalNode(
+                                                                        Rule.ADDITIVE_EXPRESSION,
+                                                                        new NonTerminalNode(
+                                                                                Rule.MULTIPLICATIVE_EXPRESSION,
+                                                                                new NonTerminalNode(
+                                                                                        Rule.PRIMARY_EXPRESSION,
+                                                                                        new NonTerminalNode(
+                                                                                                Rule.LITERAL,
+                                                                                                new NumberLiteralNode(1, 7, "3")
+                                                                                        )
+                                                                                )
+                                                                        )
+                                                                ),
+                                                                new NonTerminalNode(
+                                                                        Rule.MULTIPLICATIVE_EXPRESSION,
+                                                                        new NonTerminalNode(
+                                                                                Rule.PRIMARY_EXPRESSION,
+                                                                                new NonTerminalNode(
+                                                                                        Rule.LITERAL,
+                                                                                        new NumberLiteralNode(1, 9, "3")
+                                                                                )
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+        Assert.assertEquals(expected, result);
     }
 
     @Test
@@ -64,9 +105,31 @@ public class AutomataParserTest {
         Lexer lexer = LexerBuilder.buildTSLexer();
         Parser parser = new AutomataParser(new FileAutomata());
         List<Token> tokens = lexer.lex(input);
-        tokens.stream().map(t -> String.format("%s, %s", t.getType(), t.getLexeme())).forEach(System.out::println);
         ASTNode result = parser.parse(tokens);
-        ASTPrettyPrinter.prettyPrint(result);
+        ASTNode expected = new NonTerminalNode(
+                Rule.FILE,
+                new NonTerminalNode(
+                        Rule.PROGRAM,
+                        new NonTerminalNode(
+                                Rule.LINE,
+                                new NonTerminalNode(
+                                        Rule.STATEMENT,
+                                        new NonTerminalNode(
+                                                Rule.VARIABLE_DECLARATION,
+                                                new IdentifierNode(1, 5, "a"),
+                                                new NonTerminalNode(
+                                                        Rule.TYPE_ANNOTATION,
+                                                        new NonTerminalNode(
+                                                                Rule.TYPE,
+                                                                new NumberTypeNode(1, 9)
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
+        );
+        Assert.assertEquals(expected, result);
     }
 
     @Test
@@ -77,6 +140,52 @@ public class AutomataParserTest {
         List<Token> tokens = lexer.lex(input);
         tokens.stream().map(t -> String.format("%s, %s", t.getType(), t.getLexeme())).forEach(System.out::println);
         ASTNode result = parser.parse(tokens);
-        ASTPrettyPrinter.prettyPrint(result);
+        ASTNode firstVar = new NonTerminalNode(
+                Rule.LINE,
+                new NonTerminalNode(
+                        Rule.STATEMENT,
+                        new NonTerminalNode(
+                                Rule.VARIABLE_DECLARATION,
+                                new IdentifierNode(1, 5, "a"),
+                                new NonTerminalNode(
+                                        Rule.TYPE_ANNOTATION,
+                                        new NonTerminalNode(
+                                                Rule.TYPE,
+                                                new NumberTypeNode(1, 9)
+                                        )
+                                )
+                        )
+                )
+        );
+        ASTNode secondVar = new NonTerminalNode(
+                Rule.LINE,
+                new NonTerminalNode(
+                        Rule.STATEMENT,
+                        new NonTerminalNode(
+                                Rule.VARIABLE_DECLARATION,
+                                new IdentifierNode(1, 20, "a"),
+                                new NonTerminalNode(
+                                        Rule.TYPE_ANNOTATION,
+                                        new NonTerminalNode(
+                                                Rule.TYPE,
+                                                new NumberTypeNode(1, 24)
+                                        )
+                                )
+                        )
+                )
+        );
+
+        ASTNode expected = new NonTerminalNode(
+                Rule.FILE,
+                new NonTerminalNode(
+                        Rule.PROGRAM,
+                        new NonTerminalNode(
+                                Rule.PROGRAM,
+                                firstVar
+                        ),
+                        secondVar
+                )
+        );
+        Assert.assertEquals(expected, result);
     }
 }
