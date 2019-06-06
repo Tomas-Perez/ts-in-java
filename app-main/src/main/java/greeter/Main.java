411f9f42a92665431bceb40322664a54dd284b0d
Tomas Perez Molina <tomas.perezmolina@ing.austral.edu.ar>
2019-06-06 15:20:14 -0300

    Remove parser and lexer greeters from App main

--- app-main/src/main/java/greeter/Main.java
+++ app-main/src/main/java/greeter/Main.java
@@ -7,7 +7,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         new InterpreterGreeter().greet();
-        new LexerGreeter().greet();
-        new ParserGreeter().greet();
     }
 }
